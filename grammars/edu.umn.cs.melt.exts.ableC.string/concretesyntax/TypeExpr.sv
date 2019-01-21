2299cac5229963964be8e8732ef423e6de88eb6b
Lucas Kramer <krame505@umn.edu>
2019-01-21 16:31:20 -0600

    Utilize disambiguation class to avoid making 'string' a keyword

--- grammars/edu.umn.cs.melt.exts.ableC.string/concretesyntax/TypeExpr.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.string/concretesyntax/TypeExpr.sv
@@ -1,6 +1,11 @@
 grammar edu:umn:cs:melt:exts:ableC:string:concretesyntax;
 
-marking terminal String_t 'string' lexer classes {Ctype, Ckeyword};
+aspect parser attribute context
+  action {
+    context = addIdentsToScope([name("string", location=builtin)], String_t, context);
+  };
+
+marking terminal String_t 'string' lexer classes {Ctype, Cidentifier};
 
 concrete productions top::TypeSpecifier_c
 | 'string'
