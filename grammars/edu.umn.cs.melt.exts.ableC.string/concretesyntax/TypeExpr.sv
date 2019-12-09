eadeeec42d95c5b6808828615cee0e57d151aea2
Lucas Kramer <krame505@umn.edu>
2019-12-09 16:03:35 -0600

    Rename lexer classes

--- grammars/edu.umn.cs.melt.exts.ableC.string/concretesyntax/TypeExpr.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.string/concretesyntax/TypeExpr.sv
@@ -5,7 +5,7 @@ imports edu:umn:cs:melt:ableC:abstractsyntax:host;
 
 imports edu:umn:cs:melt:exts:ableC:string;
 
-marking terminal String_t 'string' lexer classes {Type, ScopedReserved};
+marking terminal String_t 'string' lexer classes {Type, Global};
 
 concrete productions top::TypeSpecifier_c
 | 'string'
