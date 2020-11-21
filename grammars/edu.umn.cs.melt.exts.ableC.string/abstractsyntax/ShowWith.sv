10983b0701bfc7ac02a7610cab9f8f4ba67cf8ed
Nathan Ringo <nathan@remexre.xyz>
2020-11-21 13:55:01 -0600

    Adds detection for double-declaring of show-with.

--- grammars/edu.umn.cs.melt.exts.ableC.string/abstractsyntax/ShowWith.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.string/abstractsyntax/ShowWith.sv
@@ -5,5 +5,14 @@ top::Decl ::= ty::TypeName  showFunctionName::Name
 {
   top.pp = pp"show ${ty.pp} with ${showFunctionName.pp};";
   local type::Type = ty.typerep.defaultFunctionArrayLvalueConversion;
-  forwards to defsDecl([customShowDef(type.mangledName, showFunctionName)]);
+  local localErrors::[Message] = type.errors ++
+    case getCustomShow(type, top.env) of
+    | just(_) -> [err(showFunctionName.location,
+                      show(80, pp"show for ${ty.pp} already defined"))]
+    | nothing() -> []
+    end;
+  forwards to
+    if null(localErrors)
+      then defsDecl([customShowDef(type.mangledName, showFunctionName)])
+      else warnDecl(localErrors);
 }
