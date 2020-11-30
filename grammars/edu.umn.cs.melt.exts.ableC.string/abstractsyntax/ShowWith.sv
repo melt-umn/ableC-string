6f303f87593e7d2ad850fc59e4894a770178e54c
Lucas Kramer <krame505@umn.edu>
2020-11-30 10:56:42 -0600

    Add type checking for custom show function

--- grammars/edu.umn.cs.melt.exts.ableC.string/abstractsyntax/ShowWith.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.string/abstractsyntax/ShowWith.sv
@@ -5,12 +5,18 @@ top::Decl ::= ty::TypeName  showFunctionName::Name
 {
   top.pp = pp"show ${ty.pp} with ${showFunctionName.pp};";
   local type::Type = ty.typerep.defaultFunctionArrayLvalueConversion;
-  local localErrors::[Message] = type.errors ++
+  local fnType::Type = showFunctionName.valueItem.typerep;
+  local expectedFnType::Type =
+    functionType(extType(nilQualifier(), stringType()), protoFunctionType([ty.typerep], false), nilQualifier());
+  local localErrors::[Message] = type.errors ++ showFunctionName.valueLookupCheck ++
     case getCustomShow(type, top.env) of
     | just(_) -> [err(showFunctionName.location,
                       show(80, pp"show for ${ty.pp} already defined"))]
     | nothing() -> []
-    end;
+    end ++
+    if !compatibleTypes(fnType, expectedFnType, false, false)
+    then [err(showFunctionName.location, s"show function for ${showType(type)} must have type ${showType(expectedFnType)} (got ${showType(fnType)})")]
+    else [];
   forwards to
     if null(localErrors)
       then defsDecl([customShowDef(type.mangledName, showFunctionName)])
